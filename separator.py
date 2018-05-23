533bd8789f554937eed67650f829707c4ca771e4
kur <rhdnfka94@gmail.com>
2018-05-23 23:46:07 +0900

    extract magic string literals

--- separator.py
+++ separator.py
@@ -7,18 +7,24 @@ import utils
 from fp import pipe, cmap, cfilter
 
 
+mixed_imgs_path = './data/examples'
+rgb_imgs_path = './data/rgb'
+
+utils.safe_copytree(mixed_imgs_path, rgb_imgs_path, '*.*')
+
 path2img_path = lambda path: (cv2.imread(path), path)
 is_grayscale = (lambda img: 
                   np.all(img[:,:,0] == img[:,:,1]) and 
                   np.all(img[:,:,1] == img[:,:,2]))
 
-utils.safe_copytree('./data/examples', './data/rgb', '*.*')
 f = pipe(utils.file_paths, 
          cmap(path2img_path),
          cfilter(lambda img_path: img_path[0] is not None),
          cfilter(lambda img_path: not is_grayscale(img_path[0])))
 
-for img, path in f('./data/examples'):
-    new_path = utils.make_dstpath(path, 'examples', './data/rgb')
-    print(path, new_path)
-    shutil.move(path,new_path)
+old_parent_dir = pathlib.Path(mixed_imgs_path).parts[-1]
+for img, img_path in f(mixed_imgs_path):
+    new_path = utils.make_dstpath(img_path, old_parent_dir,
+                                  rgb_imgs_path)
+    #print(img_path, old_parent_dir, new_path)
+    shutil.move(img_path,new_path)
