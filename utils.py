6c7851c0ef54155655ca1a3011fae26e8d4115ac
kur <rhdnfka94@gmail.com>
2018-07-20 16:41:25 +0900

    Generalize file_path for any dir structure

--- utils.py
+++ utils.py
@@ -42,7 +42,6 @@ def safe_copytree(srcpath, dstpath, ignores):
 def file_paths(root_dir_path):
     ''' generate file_paths of directory_path ''' 
     it = os.walk(root_dir_path)
-    next(it)
     for root,dirs,files in it:
         for path in map(lambda name:os.path.join(root,name),files):
             yield path
